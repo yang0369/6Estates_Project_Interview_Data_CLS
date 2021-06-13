4350476184c6dbebe419baa63cbaa62304160675
kewen <yang0369@e.ntu.edu.sg>
2021-06-13 15:58:56 +0800

    push the latest model and training results

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # About The Project
 
-This project is an interview assignment with a binary sentiment analysis problem.
+This project is about an interview assignment with a binary sentiment analysis problem.
 
-To address the stated problem, I have tried with a few traditional machine learning and deep learning models.
+To address the stated problem, I have tried with a few traditional NLP models.
 
 The models that I have tried are:
 
-- [ ] Distilled Bert
+- [ ] BERT model
 - [ ] TF-IDF + logistic regression
 - [ ] TF-IDF + random forest
 
@@ -28,6 +28,7 @@ The models that I have tried are:
  ┃ ┣ 📜predicted.csv
  ┃ ┣ 📜tf_model.h5
  ┃ ┗ 📜info.log
+ ┃ ┗ 📜training_logs
  ┣ 📂polyaxon
  ┃ ┣ 📂docker
  ┃ ┃ ┣ 📜experiment.df
@@ -54,61 +55,66 @@ The models that I have tried are:
 
  ┣ 📂.polyaxon
 
-- files related to polyaxon 
+> contains files related to Polyaxon experiment 
 
 ┣ 📂data
 
-- data folder, contains all the train, dev, test datasets and one word embedding file(I did not use it in this project)
-- all the data files were not uploaded to Git due to size limit
+> Data folder, contains all the train, dev, test datasets, and one word embedding file (which I did not use in this project). All the data files were not uploaded to Git due to size limit
 
  ┣ 📂model
 
-- all files related to trained model, including:  
-  									- 📜config.json: contains all the trained weights/parameters
-  									- 📜history.csv: contains the loss/accuracy during training and validation, used for plotting graph
-  									- 📜predicted.csv: contains all the predicted labels for test dataset
-  									- 📜tf_model.h5: the trained model in .h5 format
-  									- 📜info.log: records all the information during training process
+> All files generated during model training, including:  
+>
+> 				- 📜config.json: contains all the trained weights/parameters
+>
+>    - 📜history.csv: contains the loss/accuracy during training and validation, used for plotting graph
+>    - 📜predicted.csv: contains all the predicted labels for test dataset
+>    - 📜tf_model.h5: the trained model in .h5 format
+>    - 📜info.log: records all the hyperparameters/parameters during training process
+>    - 📜training_logs: records all the training epochs, train/val accuracy and loss etc.
 
  ┣ 📂polyaxon
 
-- dockerfiles for setting up the running environment for polyaxon experiment
+> Dockerfiles for setting up the running environment for polyaxon experiment
 
- ┣ 📂scripts
+┣ 📂scripts
 
-- script for linking to persistent storage on polyaxon
+> A script to setup persistent data storage on polyaxon
 
  ┣ 📂src
 
-- the source code folder, this contains the main python scripts for this project
-  - 📜config_6estate.yml: passing path and hyperparameters to the script, which makes the hyperparameter tuning easier 
-  - 📜datapipeline_6estate.py: the one-stop pipeline to run data cleaning, data preprocessing and model training, evaluation. Please refer to "User Guide" section on how to run this pipeline.
-  - 📜experiment_6estate.py: this script is created for training BERT model on Polyaxon platform, the main reason why I trained model on Polyaxon compared to local machine is the super fast training speed. It takes only about 15 minutes by the GPU on Polyaxon!
-  - 📜__init__.py: to package the folder
+> The source code folder, which contains the main python scripts for this project
+> - 📜config_6estate.yml: passing path and hyperparameters to the script, which makes the hyperparameter tuning easier 
+> - 📜datapipeline_6estate.py: the one-stop pipeline to run data cleaning, data preprocessing and model training, evaluation. Please refer to "User Guide" section on how to run this pipeline.
+> - 📜experiment_6estate.py: this script is created for training BERT model on Polyaxon platform, the main reason why I trained model on Polyaxon compared to local machine is the super fast training speed. It takes only about 15 minutes by the GPU on Polyaxon!
 
 📜analysis.ipynb 
 
-- The main analysis notebook, where a simple EDA was performed. Based on this notebook, we can know what kind of dataset out there, the basic statistics about the dataset, and also what data cleaning process should be applied. In addition, I have performed the Model result analysis on this notebook as well.
+> The main analysis notebook, where a simple EDA was performed. Based on this notebook, we can know what kind of dataset out there, the basic statistics about the dataset, and also what data cleaning process should be applied. In addition, I have performed the Model result analysis on this notebook as well.
 
  📜conda.yml
 
-- contains all the required libraries for running this project
+> Contains all the required libraries for running this project
 
 # User guide 
 
-To run the one-stop pipeline for training and evaluating BERT model, just simply go to your command prompt(windows) or terminal(linux), change directory to the project parent folder, e.g. in my case, I saved the project in the directory of "D:\personal_git\6Estates_Project_Interview_CLS\6Estates_Project_Interview_Data_CLS"
+For the purpose of simplicity, this project has one and only one python script to run the data processsing pipeline and modelling pipeline.
+
+Just simply go to your command prompt(windows) or terminal(linux), change the working directory to the project parent folder. e.g. in my case, I saved the project martials in the directory of "D:\personal_git\6Estates_Project_Interview_CLS\6Estates_Project_Interview_Data_CLS"
 
 ```bash
  cd D:\personal_git\6Estates_Project_Interview_Data_CLS\
 ```
 
-then type the python command as below:
+Then type the python command shown below to run the pipeline:
 
 ```python
 python -m src.datapipeline_6estate --config_path src/config_6estate.yml
 ```
 
-That's it, the pipeline will run itself and generate all the information, parameters, artefacts and trained model, all of them will be stored at  ┣ 📂model folder.
+That's it, the pipeline will run itself locally and generate all the modelling information, parameters, artefacts and trained model automatically, and all of them will be stored at  ┣ 📂model folder.
+
+Please take note that my model was trained on Polyaxon platform, which is why I can train the model with a very huge memory space and super fast compute speed. However, if you need to run it locally, you may need to adjust the batch size, Number of epochs etc to avoid the out of memory(OOM) issue.
 
 # About analysis.ipynb 
 
@@ -138,11 +144,12 @@ Third, I tried TF-IDF model to compare it's performance with NN model.
 
 TF-IDF is an simple and time efficient way of analyzing text data. The drawbacks is very obvious - it cannot analyze the relationship between words, also it ignores the sequence, context and grammar completely. since it is time efficient, we can take it as the benchmark of this project.
 
-I have tried two of the most commonly used model, and below are their performance(accuracy):
+I have tried two of the most commonly used model, and below are their performance (accuracy):
 
-- TF-IDF + logistic regression: 0.5103
+- TF-IDF + logistic regression: 0.51
+- TF-IDF + random forest: 0.53
 
-- TF-IDF + random forest: 0.5281
+Lastly, I have performed the result analysis for the trained BERT model, please refer to the "BERT Model Result Analysis" section for more details.
 
 # About datapipeline_6estate.py
 
@@ -179,6 +186,32 @@ In addition, to run Polyaxon experiment, we also need the config files below:
 - config_6estate.yml - pass all runtime parameters for DataPipeline and Model
 
 # BERT Model Result Analysis
+
+The trained BERT model has the best performance of ***0.88*** binary accuracy, which is much better than our benchmark model's performance.
+
+<img src="C:\Users\kewen\AppData\Roaming\Typora\typora-user-images\image-20210613150640892.png" alt="loss and accuracy graph during model training" style="zoom:50%;" />
+
+Based on the graph above, we can see the BERT model learns the training data quite fast, train and val graph crosses at epoch 2, and then the validation accuracy becomes quite stable. By test and error, I have figured out the best set of hyperparameters as below (partially shown here):
+
+- [ ] model: "bert-base-uncased"
+
+- [ ] number of labels: 1
+
+- [ ]  batch size: 128
+
+- [ ]  learning rate: 0.00005
+
+- [ ]  learning rate decay: 0.9
+
+- [ ]  number of epochs : 17
+
+- [ ]  max sentence length: 512
+
+Based on the trained model, we can get the confusion matrix as below(refer to the analysis.ipynb for more details)
+
+<img src="C:\Users\kewen\AppData\Roaming\Typora\typora-user-images\image-20210613150249212.png" alt="confusion matrix" style="zoom:50%;" />
+
+In addition, the recall is 0.9 and precision is 0.86, both of them are acceptable as expected.
 
 
 
--- src/datapipeline_6estate.py
+++ src/datapipeline_6estate.py
@@ -15,7 +15,6 @@ from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras.callbacks import ReduceLROnPlateau
 from tensorflow.keras.metrics import BinaryAccuracy
 from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
-# from transformers import AutoTokenizer, TFDistilBertForSequenceClassification
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
@@ -34,8 +33,8 @@ class DataPipeline():
             flag_contraction {bool} -- [if expand contractions for text] (default: {True})
             flag_digits {bool} -- [if remove digits from text] (default: {True})
         """
-        self.__flag_contraction = flag_contraction
-        self.__flag_digits = flag_digits
+        self._flag_contraction = flag_contraction
+        self._flag_digits = flag_digits
         logger.info("Datapipeline is initiated")
     
     @staticmethod
@@ -132,9 +131,9 @@ class DataPipeline():
         df = self.load_data(data_path)
         df = self.drop_na(df)
         df = self.drop_duplicates(df)
-        if self.__flag_contraction:
+        if self._flag_contraction:
             df.loc[:, "sentence"] = df.sentence.apply(lambda x: self.expand_contractions(x))
-        if self.__flag_digits:
+        if self._flag_digits:
             df.loc[:, "sentence"] = df.sentence.apply(lambda x: self.remove_digits(x))
         df.loc[:, "sentence"] = df.sentence.apply(lambda x: self.to_lower(x))
         logger.info("finished transforming data")        
@@ -152,16 +151,16 @@ class Model():
             config {[dict]} -- [dictionary that passes all the required path and hyperparameters]
             path {[str]} -- [where to store the trained model]
         """
-        self.__model_path = path
-        self.__tokenizer = AutoTokenizer.from_pretrained(config["model"]["MODEL"], use_fast=True)
-        self.__model = TFAutoModelForSequenceClassification.from_pretrained(config["model"]["MODEL"], num_labels=config["model"]["NUM_LABELS"])
-        self.__batch = config["model"]["BATCH_SIZE"]
-        self.__learning_rate = config["model"]["LEARNING_RATE"]
-        self.__epochs = config["model"]["EPOCHS"]
-        self.__max_length = config["model"]["MAX_LENGTH"]
-        self.__lr_patience = config["model"]["LR_PATIENCE"]
-        self.__es_patience = config["model"]["ES_PATIENCE"]
-        self.__factor = config["model"]["FACTOR"]
+        self._model_path = path
+        self._tokenizer = AutoTokenizer.from_pretrained(config["model"]["MODEL"], use_fast=True)
+        self._model = TFAutoModelForSequenceClassification.from_pretrained(config["model"]["MODEL"], num_labels=config["model"]["NUM_LABELS"])
+        self._batch = config["model"]["BATCH_SIZE"]
+        self._learning_rate = config["model"]["LEARNING_RATE"]
+        self._epochs = config["model"]["EPOCHS"]
+        self._max_length = config["model"]["MAX_LENGTH"]
+        self._lr_patience = config["model"]["LR_PATIENCE"]
+        self._es_patience = config["model"]["ES_PATIENCE"]
+        self._factor = config["model"]["FACTOR"]
         logger.info("initiating model")
 
     def preprocess_data(self, train, val, test):
@@ -175,9 +174,9 @@ class Model():
         Returns:
             [tensorflow dataset object] -- [return the tensorflow dataset by batches]
         """
-        train_encodings = self.__tokenizer(train.sentence.tolist(), truncation=True, padding=True, max_length = self.__max_length, return_tensors="tf")
-        val_encodings = self.__tokenizer(val.sentence.tolist(), truncation=True, padding=True, max_length = self.__max_length, return_tensors="tf")
-        test_encodings = self.__tokenizer(test.sentence.tolist(), truncation=True, padding=True, max_length = self.__max_length, return_tensors="tf")
+        train_encodings = self._tokenizer(train.sentence.tolist(), truncation=True, padding=True, max_length = self._max_length, return_tensors="tf")
+        val_encodings = self._tokenizer(val.sentence.tolist(), truncation=True, padding=True, max_length = self._max_length, return_tensors="tf")
+        test_encodings = self._tokenizer(test.sentence.tolist(), truncation=True, padding=True, max_length = self._max_length, return_tensors="tf")
         # prepare tensorflow dataset
         train_dataset = tf.data.Dataset.from_tensor_slices((
             dict(train_encodings),
@@ -191,39 +190,39 @@ class Model():
             dict(test_encodings),
             test.label.values
         ))
-        self.__steps_per_epoch = train_dataset.cardinality().numpy()//self.__batch 
-        self.__validation_steps = val_dataset.cardinality().numpy()//self.__batch 
+        self._steps_per_epoch = train_dataset.cardinality().numpy()//self._batch 
+        self._validation_steps = val_dataset.cardinality().numpy()//self._batch 
         # shuffle and batch tf dataset
-        dataset_train_tf = train_dataset.shuffle(train_dataset.cardinality().numpy()).batch(self.__batch)
-        dataset_val_tf = val_dataset.batch(self.__batch)
-        dataset_test_tf = test_dataset.batch(self.__batch)
+        dataset_train_tf = train_dataset.shuffle(train_dataset.cardinality().numpy()).batch(self._batch)
+        dataset_val_tf = val_dataset.batch(self._batch)
+        dataset_test_tf = test_dataset.batch(self._batch)
         # repeat and prefetch dataset 
-        self.__train_dataset = dataset_train_tf.repeat(self.__epochs).prefetch(buffer_size=AUTOTUNE)
-        self.__test_dataset = dataset_test_tf.prefetch(buffer_size=AUTOTUNE)
-        self.__validation_dataset = dataset_val_tf.prefetch(buffer_size=AUTOTUNE)
-        return self.__train_dataset, self.__test_dataset, self.__validation_dataset
+        self._train_dataset = dataset_train_tf.repeat(self._epochs).prefetch(buffer_size=AUTOTUNE)
+        self._test_dataset = dataset_test_tf.prefetch(buffer_size=AUTOTUNE)
+        self._validation_dataset = dataset_val_tf.prefetch(buffer_size=AUTOTUNE)
+        return self._train_dataset, self._test_dataset, self._validation_dataset
 
     def build_model(self):
         """[build up the BERT model]
         """
+        # make sure all layers are trainable
+        self._model.trainable = True
+        """
         # freeze the BERT encoder while training the classifier
-        for layer in self.__model.layers:
+        for layer in self._model.layers:
             if layer.name == "bert":
                 layer.trainable = False
-
-        # unfreeze the last 2 layers of encoder for training
-        for layer in self.__model.layers[:]:
+        # unfreeze the last X numbers of layers of encoder for training
+        for layer in self._model.layers[:]:
             if isinstance(layer, transformers.models.bert.modeling_tf_bert.TFBertMainLayer):
                 for idx, layer in enumerate(layer.encoder.layer):
-                    if idx in [7， 8， 9, 10, 11]:
+                    if idx in [6, 7, 8, 9, 10, 11]:
                         # print(layer.name)
                         layer.trainable = True
-
-        self.__model.compile(optimizer=Adam(learning_rate=self.__learning_rate),
+        """
+        self._model.compile(optimizer=Adam(learning_rate=self._learning_rate),
                              loss=BinaryCrossentropy(from_logits=True),
-                             metrics=BinaryAccuracy(), 
-                            ) 
-
+                             metrics=BinaryAccuracy()) 
     def fit_model(self):
         """[fit the dataset to the model and train model]
 
@@ -231,26 +230,25 @@ class Model():
             [history object] -- [records all the train and val info]
         """
         reduce_lr = ReduceLROnPlateau(
-            factor=self.__factor,
+            factor=self._factor,
             monitor='val_loss', 
             mode='min', 
             verbose=1, 
-            patience=self.__lr_patience,
+            patience=self._lr_patience,
             min_lr=1e-08)
         early_stop = EarlyStopping(
             verbose=1,
-            patience=self.__es_patience,
+            patience=self._es_patience,
             mode='auto',
             monitor='val_loss',
             restore_best_weights=True)
-        history = self.__model.fit(
-            x=self.__train_dataset,
-            validation_data=self.__validation_dataset,
-            steps_per_epoch=self.__steps_per_epoch,
-            epochs=self.__epochs,
-            validation_steps=self.__validation_steps,
-            # callbacks=[early_stop, reduce_lr])
-            callbacks=[early_stop])
+        history = self._model.fit(
+            x=self._train_dataset,
+            validation_data=self._validation_dataset,
+            steps_per_epoch=self._steps_per_epoch,
+            epochs=self._epochs,
+            validation_steps=self._validation_steps,
+            callbacks=[early_stop, reduce_lr])
         return history
 
     def evaluate_model(self):
@@ -259,7 +257,7 @@ class Model():
         Returns:
             [float] -- [return the binary accuracy]
         """
-        loss, binary_acc = self.__model.evaluate(self.__test_dataset)
+        loss, binary_acc = self._model.evaluate(self._test_dataset)
         logger.info('Test Loss: %s', loss)
         logger.info('Test Accuracy: %s', binary_acc)
         return binary_acc
@@ -267,15 +265,15 @@ class Model():
     def save_model(self):
         """[save the trained model in .h5 format]
         """
-        logger.info(f"Saving model in {self.__model_path}")
-        self.__model.save_pretrained(self.__model_path)
+        logger.info(f"Saving model in {self._model_path}")
+        self._model.save_pretrained(self._model_path)
     
     def save_predicted(self):
         """[save the predicted y labels in a csv for model performance analysis]
         """
-        predicted = [int(i) for i in tf.round(tf.sigmoid(self.__model.predict(self.__test_dataset)["logits"])).numpy()]
+        predicted = [int(i) for i in tf.round(tf.sigmoid(self._model.predict(self._test_dataset)["logits"])).numpy()]
         logger.info(f"predicted values for test_dataset: {predicted}")
-        savetxt(os.path.join(self.__model_path, "predicted.csv"), predicted, delimiter=',')
+        savetxt(os.path.join(self._model_path, "predicted.csv"), predicted, delimiter=',')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -283,24 +281,52 @@ if __name__ == "__main__":
     args = parser.parse_args()
     with open(args.config_path) as file:
         config = yaml.safe_load(file)
-    # declare drop list
-    drop_list = config["datapipe"]["drop_list"]
+    # change to local paths
+    config["dataset"]["training_path"] = "./data/train.json"
+    config["dataset"]["test_path"] = "./data/test.json"
+    config["dataset"]["val_path"] = "./data/dev.json"
+    # set the path to store trained model, training info and logger
+    path = "./model"
+    store_logs = os.path.join(path, "info.log") # where we store the logs
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+    # config logger
+    logging.basicConfig(
+        filename=store_logs,
+        filemode='w',
+        format='%(asctime)s , %(name)s - %(levelname)s : %(message)s',
+        datefmt='%Y-%m-%d %A %H:%M:%S', 
+        level=logging.INFO
+    )
+    # initiating logger
+    logger = logging.getLogger("6estate")
+    logger.info(f"parameters are:{config}")
     # initiate pipeline
     dp = DataPipeline()
-    train = dp.transform_data("./data/train.json")
+    train = dp.transform_data(config["dataset"]["training_path"])
     # remove non-English sentences as noise
-    train = train.drop(drop_list, axis=0)
-    test = dp.transform_data("./data/test.json")
-    val = dp.transform_data("./data/dev.json")
+    train = train.drop(config["datapipe"]["drop_list"], axis=0)
+    test = dp.transform_data(config["dataset"]["test_path"])
+    val = dp.transform_data(config["dataset"]["val_path"])
     train_size = train.shape[0]
     val_size = val.shape[0]
     test_size = test.shape[0]
     logger.info(f"train size:{train_size}")
     logger.info(f"test size:{test_size}")
     logger.info(f"val size:{val_size}")
+    try:
+        # Disable all GPUS
+        tf.config.set_visible_devices([], 'GPU')
+        visible_devices = tf.config.get_visible_devices()
+        for device in visible_devices:
+            assert device.device_type != 'GPU'
+    except:
+        # Invalid device or cannot modify virtual devices once initialized.
+        pass   
+    train = train.head(16)
+    val = val.head(16)
+    test = test.head(16)
     # initiating modelling
-    # set the path to store trained model
-    path = "./model"
     Bert = Model(config, path)
     Bert.preprocess_data(train, val, test)
     logger.info("Finished preprocessing input data for Bert")
